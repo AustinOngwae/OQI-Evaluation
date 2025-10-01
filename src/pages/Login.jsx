06e285da82c2cf0e3cb3552f02a1a5f68d6c408a
[dyad] <austnya@outlook.com>
2025-10-01 17:02:50 +0200

    [dyad] Enable Google sign-in - wrote 1 file(s)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
           <Auth
             supabaseClient={supabase}
             appearance={{ theme: ThemeSupa }}
-            providers={[]}
+            providers={['google']}
             theme="light"
             socialLayout="horizontal"
           />
