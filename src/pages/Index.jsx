19055aace1149eb2376b7b4794412a25bae01479
[dyad] <austnya@outlook.com>
2025-11-04 12:36:39 +0100

    [dyad] Created the missing Index.jsx file to resolve the import error. - wrote 1 file(s)

---
+++ src/pages/Index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Home from '../pages/Home'; // Assuming Home is the content for the index page
+
+const Index = () => {
+  return (
+    <Home />
+  );
+};
+
+export default Index;
