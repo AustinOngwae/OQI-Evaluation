37ff31274f1ebb723bf869ed6402dee8878ccc71
[dyad] <austnya@outlook.com>
2025-11-04 12:37:04 +0100

    [dyad] Created the missing Admin.jsx file. - wrote 1 file(s)

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AdminDashboard from '../components/questionnaire/AdminDashboard';
+
+const Admin = () => {
+  return (
+    <AdminDashboard />
+  );
+};
+
+export default Admin;
